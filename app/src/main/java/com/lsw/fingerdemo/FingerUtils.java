67064df91cb2be10d4e70e94596f65f6be97a034
pengshiping <pengshiping@163.com>
2022-03-30 19:08:04 +0800

    LswFingerDemo 提交指纹usb协议代码,添加指纹比对的代码

---
+++ app/src/main/java/com/lsw/fingerdemo/FingerUtils.java
@@ -0,0 +1,49 @@
+package com.lsw.fingerdemo;
+
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class FingerUtils {
+
+    private static String filepath = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+    public static byte[] getFeature0() {
+        try {
+            FileInputStream fin = new FileInputStream(new File(filepath + "/fingerdata/left.feat"));
+            byte[] buffer = new byte[512];
+            fin.read(buffer);
+            return buffer;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static byte[] getFeature1() {
+        try {
+            FileInputStream fin = new FileInputStream(new File(filepath + "/fingerdata/right.feat"));
+            byte[] buffer = new byte[512];
+            fin.read(buffer);
+            return buffer;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static byte[] getFingerImage() {
+        try {
+            FileInputStream fin = new FileInputStream(new File(filepath + "/fingerdata/image.dat"));
+            byte[] buffer = new byte[512];
+            fin.read(buffer);
+            return buffer;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
